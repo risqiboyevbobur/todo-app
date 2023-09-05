7f490c89eb7b397e81831931229bc19927da96f0
risqiboyevbobur <brisqiboyev00@gmail.com>
2023-09-05 22:22:34 +0500

    3 and 4 video completed successfully

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
 
 	"github.com/risqiboyevbobur/todo-app.git"
 	"github.com/risqiboyevbobur/todo-app.git/pkg/handler"
+	"github.com/risqiboyevbobur/todo-app.git/pkg/repository"
+	"github.com/risqiboyevbobur/todo-app.git/pkg/service"
 )
 
-func main()  {
-	handlers := new(handler.Handler)
-	srv:= new(todo.Server)
-	if err := srv.Run("8080",handlers.InitRoutes()); err != nil{
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initilization: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	service := service.NewService(repos)
+	handlers := handler.NewHandler(service)
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while running: %s", err.Error())
 	}
 	fmt.Println("Hello")
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.down.sql
@@ -0,0 +1,5 @@
+drop table users;
+drop table todo_lists;
+drop table user_lists;
+drop table todo_items;
+drop table lists_items;
