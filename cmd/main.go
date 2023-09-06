19c17d28bd8bfa783543a99d948dd54c3e35c4b7
risqiboyevbobur <brisqiboyev00@gmail.com>
2023-09-06 15:05:53 +0500

    5 - video completed

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 
 	"github.com/risqiboyevbobur/todo-app.git"
@@ -16,14 +17,27 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initilization: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+	if err:= godotenv.Load(); err != nil {
+		log.Fatalf("error loading env varible :%s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:      viper.GetString("db.port"),
+		Username:  viper.GetString("db.username"),
+		Password:  viper.GetString("db.password"),
+		DBName:    viper.GetString("db.dbname"),
+		SSLMode:   viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		// log.Fatalf("failed to initiliazed db:%s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	service := service.NewService(repos)
 	handlers := handler.NewHandler(service)
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while running: %s", err.Error())
 	}
-	fmt.Println("Hello")
 }
 
 func initConfig() error {
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -31,3 +31,4 @@ CREATE TABLE lists_items
     item_id int references todo_items(id) on delete cascade not null,
     list_id int references todo_lists(id) on delete cascade not null,
 );
+    
